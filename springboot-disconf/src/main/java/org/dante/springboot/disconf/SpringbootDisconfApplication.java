a08baabdb11d49c832eefed246bee2fde83765f2
dante <sunchao.0129@163.com>
2017-11-10 09:23:40 +0800

    测试Disconf

---
+++ springboot-disconf/src/main/java/org/dante/springboot/disconf/SpringbootDisconfApplication.java
@@ -0,0 +1,14 @@
+package org.dante.springboot.disconf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDisconfApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDisconfApplication.class, args);
+	}
+	
+	
+}
---
+++ springboot-disconf/src/test/java/org/dante/springboot/disconf/SpringbootDisconfApplicationTests.java
@@ -0,0 +1,12 @@
+package org.dante.springboot.disconf;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDisconfApplicationTests {
+
+
+}
