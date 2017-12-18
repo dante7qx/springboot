5f6a1549528e84e2d87aa2196e537d493d97d0ef
dante <sunchao.0129@163.com>
2017-12-18 18:51:55 +0800

    修改

--- springboot-poi/src/main/java/org/dante/springboot/SpringbootPoiApplication.java
+++ springboot-poi/src/main/java/org/dante/springboot/SpringbootPoiApplication.java
@@ -1,7 +1,11 @@
 package org.dante.springboot;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
@@ -10,5 +14,12 @@ public class SpringbootPoiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootPoiApplication.class, args);
+	}
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setLocation("/Users/dante/Documents/Project/spring/springboot/springboot-poi/src/main/resources/tmp");
+		return factory.createMultipartConfig();
 	}
 }
