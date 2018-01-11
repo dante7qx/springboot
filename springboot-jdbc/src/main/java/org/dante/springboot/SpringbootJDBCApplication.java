88cbdd229af4b770e1ae7da5321c501e6cc25224
dante <sunchao.0129@163.com>
2018-01-11 14:49:39 +0800

    添加JdbcTemplate示例

--- springboot-jdbc/src/main/java/org/dante/springboot/SpringbootJDBCApplication.java
+++ springboot-jdbc/src/main/java/org/dante/springboot/SpringbootJDBCApplication.java
@@ -3,6 +3,13 @@ package org.dante.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 参考：
+ * https://www.cnblogs.com/tomlxq/p/5514658.html
+ * 
+ * @author dante
+ *
+ */
 @SpringBootApplication
 public class SpringbootJDBCApplication {
 
