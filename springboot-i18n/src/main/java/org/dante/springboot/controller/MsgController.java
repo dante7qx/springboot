5541e06eb9daf4a40efa4d00f4868225e0d9e12c
dante <sunchao.0129@163.com>
2018-08-19 22:07:55 +0800

    Springboot I18N 国际化

--- springboot-i18n/src/main/java/org/dante/springboot/controller/MsgController.java
+++ springboot-i18n/src/main/java/org/dante/springboot/controller/MsgController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.dante.springboot.service.I18nMsgService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,26 @@ public class MsgController {
 	
 	@Autowired
 	private I18nMsgService i18nMsgService;
+	
+	/**
+	 * Springboot I18N 国际化
+	 * 
+	 * @return
+	 */
+	@GetMapping(value = "", produces = MediaType.TEXT_HTML_VALUE)
+	public String i18n() {
+		return "<h1 align='center'>Springboot i18n 国际化</h1>";
+	}
+	
+	/**
+	 * 健康检查
+	 * 
+	 * @return
+	 */
+	@GetMapping("/healthz")
+	public String healthz() {
+		return "ok";
+	}
 
 	/**
 	 * 添加 Header 信息
@@ -22,8 +43,9 @@ public class MsgController {
 	 */
 	@GetMapping("/msg")
 	public String msg(HttpServletRequest request) {
-		return i18nMsgService.getMessage("welcome");
+		return i18nMsgService.getMessage("welcome").concat(" - ").concat(i18nMsgService.getMessage("country"));
 	}
+	
 	
 	
 }
