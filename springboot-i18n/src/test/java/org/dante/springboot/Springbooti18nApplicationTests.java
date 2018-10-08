e3b6b92e22fd3c927ce06c6c809eaf80ca54d6cf
dante <sunchao.0129@163.com>
2018-10-08 14:29:16 +0800

    添加国际化单元测试

--- springboot-i18n/src/test/java/org/dante/springboot/Springbooti18nApplicationTests.java
+++ springboot-i18n/src/test/java/org/dante/springboot/Springbooti18nApplicationTests.java
@@ -1,11 +1,40 @@
 package org.dante.springboot;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class Springbooti18nApplicationTests {
 	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void msgTest() throws Exception {
+		this.mvc.perform(get("/msg").header("Accept-Language", "zh-cn"))
+				.andExpect(status().isOk())
+				.andDo(new ResultHandler() {
+					@Override
+					public void handle(MvcResult result) throws Exception {
+						log.info(result.getResponse().getContentAsString());
+					}
+				});
+	}
+
 }
