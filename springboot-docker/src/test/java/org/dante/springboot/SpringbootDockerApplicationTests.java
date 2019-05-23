28e61c9d16aea57a3940640cd16cbdb17c49d823
dante <sunchao.0129@163.com>
2019-05-23 09:51:49 +0800

    添加测试用例

--- springboot-docker/src/test/java/org/dante/springboot/SpringbootDockerApplicationTests.java
+++ springboot-docker/src/test/java/org/dante/springboot/SpringbootDockerApplicationTests.java
@@ -1,16 +1,37 @@
 package org.dante.springboot;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.dante.springboot.docker.DockerController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
 public class SpringbootDockerApplicationTests {
 	
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new DockerController()).build();
+	}
+	
 	@Test
-	public void test() {
-		
+	public void test() throws Exception {
+		RequestBuilder request = get("/docker");
+		mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(containsString("Docker")));
 	}
 }
