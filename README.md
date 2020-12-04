0fd13396473bc726ea28c4571f914e9e8c3a91f7
dante7qx <sunchao.0129@163.comm>
2020-12-04 23:50:17 +0800

    Springboot 2.4.x 版本

---
+++ README.md
@@ -0,0 +1,16 @@
+# Spring Boot 2.4.0
+https://www.oschina.net/news/120443/spring-boot-2-4-0-released
+
+## 一, 更新记录
+Spring Boot 2.4.0 正式发布，现在可以从 maven 中央仓库获取，全新的版本号定义，正式版本不再添加 RELEASE 后缀声明。
+
+  <dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-dependencies</artifactId>
+    <version>2.4.0</version>
+    <type>pom</type>
+    <scope>import</scope>
+  </dependency>
+
+
+
--- springboot-richtext/src/test/java/org/dante/springboot/SpringbootRichTextEditorApplicationTests.java
+++ springboot-richtext/src/test/java/org/dante/springboot/SpringbootRichTextEditorApplicationTests.java
@@ -1,11 +1,13 @@
 package org.dante.springboot;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootRichTextEditorApplicationTests {
 
+	@Test
+	public void test() {
+		System.out.println(111);
+	}
 }
