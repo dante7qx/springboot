29537c51f5eb1780a66e79097917a68536c27164
dante7qx <sunchao.0129@163.comm>
2022-05-08 20:53:10 +0800

    测试多线程批量导入

--- springboot-hikaricp/src/test/java/org/dante/springboot/SpringbootHikariCPApplicationTests.java
+++ springboot-hikaricp/src/test/java/org/dante/springboot/SpringbootHikariCPApplicationTests.java
@@ -1,38 +1,8 @@
 package org.dante.springboot;
 
-import java.util.List;
-
-import org.dante.springboot.dao.HobbyDAO;
-import org.dante.springboot.dao.PersonDAO;
-import org.dante.springboot.po.HobbyPO;
-import org.dante.springboot.po.PersonPO;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SpringbootHikariCPApplicationTests {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootHikariCPApplicationTests.class);
-	
-	@Autowired
-	protected PersonDAO personDAO;
-	@Autowired
-	protected HobbyDAO hobbyDAO;
-	
-	@Test
-	public void findPersons() {
-		List<PersonPO> persons = personDAO.findAll();
-		LOGGER.info(persons.toString());
-	}
-	
-	@Test
-	public void findHobbys() {
-		List<HobbyPO> hobbys = hobbyDAO.findAll();
-		LOGGER.info(hobbys.toString());
-	}
-	
-
 }
