5958c0534ff5fec8450ef6ae05159cd4ab8d42f8
dante7qx <sunchao.0129@163.comm>
2022-09-21 11:33:55 +0800

    添加数据库文档生成示例

---
+++ springboot-db-doc/src/main/java/org/dante/springboot/SpringbootDBDocApplication.java
@@ -0,0 +1,20 @@
+package org.dante.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 多数据源
+ * https://www.baeldung.com/spring-boot-configure-multiple-datasources
+ * 
+ * @author dante
+ *
+ */
+@SpringBootApplication
+public class SpringbootDBDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDBDocApplication.class, args);
+	}
+	
+}
