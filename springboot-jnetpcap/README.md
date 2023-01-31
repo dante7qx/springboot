00d29fadae3836b3da76f9e166ce0982d55c6157
dante7qx <sunchao.0129@163.comm>
2023-01-31 11:29:17 +0800

    springboot网络抓包示例

---
+++ springboot-jnetpcap/README.md
@@ -0,0 +1,12 @@
+## 基于jnetpcap进行抓包
+
+### 一.  概述
+
+### 二.  技术原理
+
+### 三.  开发说明
+
+### 四.  参考资料 
+
+- https://blog.csdn.net/m0_37892044/article/details/120085261
+- https://blog.csdn.net/m0_37892044/article/details/120151541
---
+++ springboot-jnetpcap/src/main/java/org/dante/springboot/SpringbootJnetpcapApplication.java
@@ -0,0 +1,12 @@
+package org.dante.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJnetpcapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJnetpcapApplication.class, args);
+	}
+}
